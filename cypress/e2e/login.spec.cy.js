7b42adc86deda153b3ef2f3ae12ec82c88287555
Angela Faverani <yago.54@hotmail.com>
2024-11-05 23:11:46 -0300

    Added Selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,27 +1,26 @@
 describe("Orange HRM Test", () => {
- 
- const selectorsList = {
-  usernameFilde: "[name='username']",
-  passwordFilde: "[name='password']",
-  loginButton: "[type='submit']",
-  SelectionTitleTopBar:".oxd-topbar-header-breadcrumb-module",
- wrongCredencialAlert: "[role='alert']"
- }
- 
+  const selectorsList = {
+    usernameField: '[name="username"]',
+    passwordField: '[name="password"]',
+    loginButton: '[type="submit"]',
+    SelectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+    wrongCredentialAlert: "[role='alert']" // Corrigido o nome da propriedade
+  };
+
   it("login - Success", () => {
     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-    cy.get("[name='username']").type("Admin"); // Digita "Admin" no campo de nome de usuário
-    cy.get("[name='password']").type("admin123"); // Digita "admin123" no campo de senha
-    cy.get("[type='submit']").click(); // Clica no botão de submit
+    cy.get(selectorsList.usernameField).type("Admin"); // Digita "Admin" no campo de nome de usuário
+    cy.get(selectorsList.passwordField).type("admin123"); // Digita "admin123" no campo de senha
+    cy.get(selectorsList.loginButton).click(); // Clica no botão de submit
     cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
-    cy.get(".oxd-topbar-header-breadcrumb-module").contains("Dashboard");
+    cy.get(selectorsList.SelectionTitleTopBar).contains("Dashboard");
   });
+
   it("login - fail", () => {
     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-    cy.get("[name='username']").type("Test"); // verificando Admin
-    cy.get("[name='password']").type("Test"); // verificando password
-    cy.get("[type='submit']").click(); // clicar botão login
-    cy.get("[role='alert']"); // Seleciona o elemento com o atributo role='alert'
+    cy.get(selectorsList.usernameField).type("Test"); // verificando Admin
+    cy.get(selectorsList.passwordField).type("Test"); // verificando password
+    cy.get(selectorsList.loginButton).click(); // clicar botão login
+    cy.get(selectorsList.wrongCredentialAlert).should('exist'); // Verifica se o alerta de credenciais incorretas aparece
   });
 });
-
